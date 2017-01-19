1d32bec6811f07a7b887a5f8c2a7244ede3f4050
Dan <danielpowell12@msn.com>
2017-01-19 08:46:08 -0700

    Sends root_path to words#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :words
+  root to: "words#index"
 
+  resources :words
   get 'todays_word', to: 'words#todays_word'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
